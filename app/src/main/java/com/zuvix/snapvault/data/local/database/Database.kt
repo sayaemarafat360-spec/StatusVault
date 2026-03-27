7e1879dae3be8b4328bdaf30d8fe485ce2efee14
Sayaem Arafat <sayaemarafat360@gmail.com>
2026-03-28 00:30:22 +0600

    First

--- app/src/main/java/com/zuvix/snapvault/data/local/database/Database.kt
+++ app/src/main/java/com/zuvix/snapvault/data/local/database/Database.kt
@@ -2,7 +2,6 @@ package com.zuvix.snapvault.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 
 @Database(
     entities = [
@@ -14,7 +13,6 @@ import androidx.room.TypeConverters
     version = 1,
     exportSchema = false
 )
-@TypeConverters(Converters::class)
 abstract class SnapVaultDatabase : RoomDatabase() {
     abstract fun savedStatusDao(): SavedStatusDao
     abstract fun statusViewHistoryDao(): StatusViewHistoryDao
